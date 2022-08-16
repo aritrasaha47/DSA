a34750881a92ae7a482152c3bd48e15c2a8988e1
Aritra Saha <74985272+aritrasahacodes@users.noreply.github.com>
2022-08-17 00:43:52 +0530

    Time: 668 ms (5.01%), Space: 56.9 MB (5.00%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -8,25 +8,21 @@ public:
     }
 
     map<int,int> ::iterator it;
-
-
-
-    // for(it=m.begin();it!=m.end();it++)
-    // {
-    //     cout<<it->first<<" "<<it->second<<endl;
-    // }
-    it=m.end();
-    if(k>nums.size())
+    for(it=m.begin();it!=m.end();it++)
     {
-        it--;
+        cout<<it->first<<" "<<it->second<<endl;
     }
-    else
+    it=m.end();
+    // if(k>nums.size())
+    // {
+    //     it--;
+    // }
+    if(k<=nums.size())
     {
         while(k>0)
         {
             it--;
             k-=it->second;
-            //cout<<k<<" ";
         }
     }
     return it->first;
