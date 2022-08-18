4242b7e1bcdb8bbc4b87281ff03df083f57c983e
Aritra Saha <74985272+aritrasahacodes@users.noreply.github.com>
2022-08-18 23:44:01 +0530

    Time: 197 ms (95.78%), Space: 78.3 MB (63.73%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -1,44 +1,24 @@
 class Solution {
 public:
-    static bool cmp(pair<int, int>& a,pair<int, int>& b)
-    {
-        return a.second > b.second;
-    }
-  
-// Function to sort the map according
-// to value in a (key-value) pairs
-    int mapsort(map<int, int>& M,int s)
-    {
-  
-        // Declare vector of pairs
-        vector<pair<int, int> > A;
-        vector <int> ans;
-        // Copy key-value pair from Map
-        // to vector of pairs
-        for (auto& it : M) 
-        {
-            A.push_back(it);
-        }
-  
-        // Sort using comparator function
-        sort(A.begin(), A.end(), cmp);
-  
-        for (auto& it : A) 
-        {
-        ans.push_back(it.first);
-            s-=it.second;
-            if(s<=0)
-                break;
-        }
-        return ans.size();
-    }
     int minSetSize(vector<int>& arr) {
-        map <int,int> m;
+        unordered_map<int,int> m;
+        int half=arr.size()/2;
+        vector <int> ans;
         for(int i=0;i<arr.size();i++)
         {
             m[arr[i]]++;
         }
-        return mapsort(m,arr.size()/2);
-
+        for(auto& it:m)
+        {
+            ans.push_back(it.second);
+        }
+        sort(ans.begin(),ans.end(),greater <int>());
+        int count=0;
+        while(half>0)
+        {
+            half-=ans[count];
+            count++;
+        }
+        return count;
     }
 };
