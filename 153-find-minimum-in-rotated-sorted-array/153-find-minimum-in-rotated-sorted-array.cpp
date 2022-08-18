bf282b86b1111aa1c4409168492d10770a8fd3e1
Aritra Saha <74985272+aritrasahacodes@users.noreply.github.com>
2022-08-19 01:17:20 +0530

    Time: 6 ms (53.25%), Space: 10.3 MB (23.20%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,39 +1,32 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        int l=0;
-        int h=nums.size()-1;
-        while(l<=h)
+    int l=0;
+    int h=nums.size()-1;
+    if(nums[l]<=nums[h])
+    {
+        return nums[0];
+    }
+    while(l<=h)
+    {
+        int mid=(l+h)/2;
+        if(nums[mid]>nums[mid+1])
         {
-            int mid=(l+h)/2;
-            cout<<l<<" "<<h<<" "<<mid<<endl;
-            if(l==h)
-                return nums[l];
-            else if(nums[l]<=nums[mid])
-            {
-                //First part is sorted
-                if(nums[mid+1]>nums[mid] && nums[h]>nums[l])
-                {
-                    h=mid;
-                }
-                else
-                {
-                    l=mid+1;
-                }
-            }
-            else
-            {
-                //Second part is sorted
-                if(nums[mid-1]>nums[mid] && nums[l]>nums[mid])
-                {
-                    l=mid;
-                }
-                else
-                {
-                    h=mid-1;
-                }
-            }
+            return nums[mid+1];
         }
-        return -1;
+        else if(nums[mid]<nums[mid-1])
+        {
+            return nums[mid];
+        }
+        else if(nums[l]<=nums[mid])
+        {
+            l=mid+1;
+        }
+        else if(nums[mid]<=nums[h])
+        {
+            h=mid-1;
+        }
+    }
+    return -1;
     }
 };
