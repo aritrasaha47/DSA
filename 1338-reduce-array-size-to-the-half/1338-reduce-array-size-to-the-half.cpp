91cbfad67368c8b9025473e83e717393d69d51af
Aritra Saha <74985272+aritrasahacodes@users.noreply.github.com>
2022-08-18 23:32:02 +0530

    Time: 495 ms (16.58%), Space: 84.5 MB (6.65%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -7,12 +7,12 @@ public:
   
 // Function to sort the map according
 // to value in a (key-value) pairs
-    vector <pair<int, int> > mapsort(map<int, int>& M)
+    int mapsort(map<int, int>& M,int s)
     {
   
         // Declare vector of pairs
         vector<pair<int, int> > A;
-  
+        vector <int> ans;
         // Copy key-value pair from Map
         // to vector of pairs
         for (auto& it : M) 
@@ -23,26 +23,7 @@ public:
         // Sort using comparator function
         sort(A.begin(), A.end(), cmp);
   
-        // Print the sorted value
-//         for (auto& it : A) 
-//         {
-  
-//         cout << it.first << ' '
-//              << it.second << endl;
-//         }
-        return A;
-    }
-    int minSetSize(vector<int>& arr) {
-        map <int,int> m;
-        vector<int> ans;
-        int s=arr.size()/2;
-        vector<pair<int, int> > a;
-        for(int i=0;i<arr.size();i++)
-        {
-            m[arr[i]]++;
-        }
-        a=mapsort(m);
-        for (auto& it : a) 
+        for (auto& it : A) 
         {
         ans.push_back(it.first);
             s-=it.second;
@@ -50,5 +31,14 @@ public:
                 break;
         }
         return ans.size();
+    }
+    int minSetSize(vector<int>& arr) {
+        map <int,int> m;
+        for(int i=0;i<arr.size();i++)
+        {
+            m[arr[i]]++;
+        }
+        return mapsort(m,arr.size()/2);
+
     }
 };
